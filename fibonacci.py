3db952b76f1862d347b7927cdc3b7032469a92c1
satuhin95 <saifcm95@gmail.com>
2023-06-15 12:26:06 +0600

    fibinacci

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number "));
+def fibinacci(n):
+    if n == 0:
+        return 0
+    else:
+        x = 0
+        y = 1
+        print(x)    
+        print(y)    
+        for i in range(1,n):
+            z = x + y
+            x = y
+            y = z
+            print(z)
+fibinacci(n)            
---
+++ pattern1.py
@@ -0,0 +1,12 @@
+row = int(input('Enter a Number  '))
+
+count = 0;
+for i in range(0,row):
+    for j in range(0, row -i -1):
+        print(end=" ")
+
+    count = count + 1
+    for k in range(0, i+count):
+        print('*', end="")
+
+    print(" ")        
